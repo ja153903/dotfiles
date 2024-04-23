4996fa19144f99f4c3a9bd987b9dc75c770cd404
ja153903 <jaimeabbariao@gmail.com>
2024-04-23 01:22:42 -0400

    feat: using float again

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -57,7 +57,7 @@ return {
         hijack_netrw_behavior = "disabled",
       },
       window = {
-        position = "left",
+        position = "float",
         mappings = {
           ["<space>"] = "none",
         },
