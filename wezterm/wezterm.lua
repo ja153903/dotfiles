8efd770282c010fedbb6750969a64889eac9255b
ja153903 <jaimeabbariao@gmail.com>
2024-04-27 18:01:46 -0400

    updating font_size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,7 +6,7 @@ local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
 config.font = wezterm.font_with_fallback({ "Gintronic", "nonicons" })
-config.font_size = 14
+config.font_size = 13
 config.enable_scroll_bar = false
 config.window_padding = {
 	left = 0,
