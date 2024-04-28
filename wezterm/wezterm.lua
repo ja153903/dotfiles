186a0af9834ee2bc53a91febed96d068178ef0a1
ja153903 <jaimeabbariao@gmail.com>
2024-04-28 13:47:51 -0400

    updating keymaps for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -36,12 +36,12 @@ config.color_scheme = "OLEDppuccin"
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
 	{
-		key = "p",
+		key = "n",
 		mods = "LEADER",
 		action = wezterm.action.ActivateTabRelative(1),
 	},
 	{
-		key = "n",
+		key = "p",
 		mods = "LEADER",
 		action = wezterm.action.ActivateTabRelative(-1),
 	},
