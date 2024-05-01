514bb8f73ac2a4d7f3682b8b5d0fd9d70c5900c3
ja153903 <jaimeabbariao@gmail.com>
2024-05-01 19:13:20 -0400

    updating colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -143,7 +143,37 @@ return {
       end,
     },
   },
-  { "miikanissi/modus-themes.nvim", priority = 1000 },
+  {
+    "miikanissi/modus-themes.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      variant = "tinted", -- Theme comes in four variants `default`, `tinted`, `deuteranopia`, and `tritanopia`
+      transparent = false, -- Transparent background (as supported by the terminal)
+      dim_inactive = false, -- "non-current" windows are dimmed
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = false, bold = true },
+        functions = { bold = true },
+        variables = {},
+      },
+
+      --- You can override specific color groups to use other groups or a hex color
+      --- function will be called with a ColorScheme table
+      ---@param colors ColorScheme
+      on_colors = function(colors) end,
+
+      --- You can override specific highlights to use other groups or a hex color
+      --- function will be called with a Highlights and ColorScheme table
+      ---@param highlights Highlights
+      ---@param colors ColorScheme
+      on_highlights = function(highlights, colors)
+        highlights['@tag.tsx'] = { fg = '#00eff0', style = 'bold' }
+      end,
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
