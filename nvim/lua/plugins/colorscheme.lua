565cf17267a50759990f42ff195636f3c8ddd37a
ja153903 <jaimeabbariao@gmail.com>
2024-05-03 19:50:37 -0400

    removing some default stuff

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -125,23 +125,6 @@ return {
         h5 = "pine",
         h6 = "foam",
       },
-
-      highlight_groups = {
-        Comment = { fg = "foam" },
-        VertSplit = { fg = "muted", bg = "muted" },
-      },
-
-      before_highlight = function(group, highlight, palette)
-        -- Disable all undercurls
-        if highlight.undercurl then
-          highlight.undercurl = false
-        end
-
-        -- Change palette colour
-        if highlight.fg == palette.pine then
-            highlight.fg = palette.foam
-        end
-      end,
     },
   },
   {
