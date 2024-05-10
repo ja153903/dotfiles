16dc4a8f8eee93ddb8f1e9a03f48ec5f625f5b35
ja153903 <jaimeabbariao@gmail.com>
2024-05-09 20:10:59 -0400

    updating colorscheme again

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -24,18 +24,17 @@ config.tab_max_width = 64
 -- config.colors = colors
 -- config.window_frame = window_frame
 config.line_height = 1.5
-config.color_scheme = 'zenbones_dark'
--- -- config.color_scheme = 'Modus-Vivendi'
--- local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
--- custom.background = "#0E0E0E"
--- custom.tab_bar.background = "#040404"
--- custom.tab_bar.inactive_tab.bg_color = "#0f0f0f"
--- custom.tab_bar.new_tab.bg_color = "#080808"
---
--- config.color_schemes = {
--- 	["OLEDppuccin"] = custom,
--- }
--- config.color_scheme = "OLEDppuccin"
+
+local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
+custom.background = "#191919"
+custom.tab_bar.background = "#040404"
+custom.tab_bar.inactive_tab.bg_color = "#0f0f0f"
+custom.tab_bar.new_tab.bg_color = "#080808"
+
+config.color_schemes = {
+	["OLEDppuccin"] = custom,
+}
+config.color_scheme = "OLEDppuccin"
 
 -- timeout_milliseconds defaults to 1000 and can be omitted
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
