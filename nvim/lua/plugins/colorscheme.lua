18ce8ffef65f3093af588acc7919f2b251b83862
ja153903 <jaimeabbariao@gmail.com>
2024-05-09 20:10:03 -0400

    updating with new colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -210,14 +210,21 @@ return {
     end,
   },
   {
-    "kdheepak/monochrome.nvim",
+    "mcchrish/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = "rktjmp/lush.nvim",
     lazy = false,
-    priority = 1000
+    priority = 1000,
+    config = function()
+      vim.g.zenwritten = { darkness = "dark" }
+    end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "monochrome",
+      colorscheme = "zenwritten",
     },
   },
 }
