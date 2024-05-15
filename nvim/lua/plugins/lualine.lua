fa3c02d19a2e4a047d18e6ab4728e0eb1e0844f5
ja153903 <jaimeabbariao@gmail.com>
2024-05-15 14:49:12 -0400

    updating some stuff

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -8,15 +8,15 @@ local lualine = require('lualine')
 local colors = {
   bg       = '#232136',
   fg       = '#e0def4',
-  yellow   = '#ECBE7B',
-  cyan     = '#008080',
-  darkblue = '#081633',
+  yellow   = '#f6c177',
+  cyan     = '#9ccfd8',
+  darkblue = '#3e8fb0',
   green    = '#98be65',
-  orange   = '#FF8800',
+  orange   = '#ea9a97',
   violet   = '#a9a1e1',
   magenta  = '#c678dd',
   blue     = '#51afef',
-  red      = '#ec5f67',
+  red      = '#eb6f92',
 }
 
 local conditions = {
