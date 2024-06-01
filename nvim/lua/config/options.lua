590c8b589974ee6ff5c9a83bb337b13efab4a0af
ja153903 <jaimeabbariao@gmail.com>
2024-06-01 00:34:04 -0400

    removing old neovim config
    currently using another config that is taken care of elsewhere

--- nvim/lua/config/options.lua
+++
@@ -1,9 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
-vim.opt.swapfile = false
-vim.opt.cursorline = true
-vim.g.autoformat = false
-vim.g.root_spec = { "cwd" }
-
