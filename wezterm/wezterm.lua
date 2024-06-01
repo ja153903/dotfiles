9b584b40a3eeb7d9d26f09803188289e27b7dad9
ja153903 <jaimeabbariao@gmail.com>
2024-06-01 00:33:07 -0400

    adding more line height because i cant see stuff anymore

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -23,7 +23,7 @@ config.tab_bar_at_bottom = true
 config.tab_max_width = 64
 -- config.colors = colors
 -- config.window_frame = window_frame
-config.line_height = 1.2
+config.line_height = 1.4
 -- config.color_scheme = 'DoomOne'
 config.color_scheme = "Solarized Dark - Patched"
 config.window_background_opacity = 1.0
@@ -32,27 +32,27 @@ local custom = wezterm.color.get_builtin_schemes()["Solarized Dark - Patched"]
 custom.background = "#00141A"
 custom.foreground = "#FFF"
 custom.tab_bar = {
-  background = "#00141A",
-  active_tab = {
-    bg_color = "#063642",
-    fg_color = "#FFF"
-  },
-  inactive_tab = {
-    bg_color = "#00141A",
-    fg_color = "#FFF"
-  },
-  new_tab = {
-    bg_color = "#002B36",
-    fg_color = "#EEE8D5"
-  }
+	background = "#00141A",
+	active_tab = {
+		bg_color = "#063642",
+		fg_color = "#FFF",
+	},
+	inactive_tab = {
+		bg_color = "#00141A",
+		fg_color = "#FFF",
+	},
+	new_tab = {
+		bg_color = "#002B36",
+		fg_color = "#EEE8D5",
+	},
 }
 
 config.color_schemes = { ["Solarized Osaka"] = custom }
 config.color_scheme = "Solarized Osaka"
 
 config.inactive_pane_hsb = {
-  saturation = 1.0,
-  brightness = 1.0,
+	saturation = 1.0,
+	brightness = 1.0,
 }
 
 -- local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
