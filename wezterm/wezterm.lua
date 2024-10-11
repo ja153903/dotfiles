12b5902c28c2c1e60c7b8228651d87b6cbd4c76a
ja153903 <jaimeabbariao@gmail.com>
2024-10-11 16:15:58 -0400

    updating wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -27,7 +27,7 @@ config.line_height = 1.3
 -- config.macos_window_background_blur = 20
 
 config.color_schemes = color_schemes
-config.color_scheme = "Solarized Osaka"
+config.color_scheme = "nightfox"
 -- config.colors = rose_pine_theme.colors()
 -- config.window_frame = rose_pine_theme.window_frame()
 
