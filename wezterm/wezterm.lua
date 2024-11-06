613505798dfaa97fd550005cbc7ee95b375f5634
ja153903 <jaimeabbariao@gmail.com>
2024-11-05 22:52:20 -0500

    updating stuff

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,14 +1,13 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
-local rose_pine_theme = wezterm.plugin.require("https://github.com/neapsix/wezterm").main
 local color_schemes = require("lua/color_schemes")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
-config.font = wezterm.font_with_fallback({ "MonoLisa", "nonicons" })
+config.font = wezterm.font_with_fallback({ "MonoLisa Nerd Font", "nonicons" })
 config.font_size = 13
 config.freetype_load_flags = "NO_HINTING"
 config.enable_scroll_bar = false
@@ -23,13 +22,8 @@ config.tab_bar_at_bottom = true
 config.tab_max_width = 64
 config.line_height = 1.3
 
--- config.window_background_opacity = 0.9
--- config.macos_window_background_blur = 20
-
 config.color_schemes = color_schemes
-config.color_scheme = "nightfox"
--- config.colors = rose_pine_theme.colors()
--- config.window_frame = rose_pine_theme.window_frame()
+config.color_scheme = "Catppuccin Mocha"
 
 config.inactive_pane_hsb = {
   saturation = 1.0,
