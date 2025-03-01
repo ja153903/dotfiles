e4bcf52eaffd21a61db69c39de815d837fcdaf54
ja153903 <jaimeabbariao@gmail.com>
2025-02-28 19:59:23 -0500

    updating code

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -14,3 +14,19 @@ end
 set_filetype_settings("cpp", 4, 4)
 set_filetype_settings("c", 4, 4)
 set_filetype_settings("roc", 4, 4)
+
+-- make .roc files have the correct filetype
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = { "*.roc" },
+  command = "set filetype=roc",
+})
+
+-- add roc tree-sitter
+local parsers = require("nvim-treesitter.parsers").get_parser_configs()
+
+parsers.roc = {
+  install_info = {
+    url = "https://github.com/faldor20/tree-sitter-roc",
+    files = { "src/parser.c", "src/scanner.c" },
+  },
+}
--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+if vim.g.vscode then
+  vim.keymap.del("n", "<leader>e")
+  vim.keymap.del("n", "<leader>l")
+  vim.keymap.del("n", "<leader>sg")
+end
+
