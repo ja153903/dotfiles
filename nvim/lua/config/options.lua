bc07fff168e7b766bd5a21f7837e956aea719cef
ja153903 <jaimeabbariao@gmail.com>
2025-03-09 01:24:33 -0500

    using github github_light

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,5 +4,3 @@
 vim.hl = vim.highlight
 vim.opt.list = false
 vim.opt.termguicolors = true
-
-vim.opt.mouse = ""
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,24 @@
 return {
+  -- {
+  --   "sainnhe/sonokai",
+  --   config = function()
+  --     vim.g.sonokai_style = "andromeda"
+  --     vim.g.sonokai_better_performance = 1
+  --     vim.cmd([[colorscheme sonokai]])
+  --   end,
+  -- },
+
   {
-    "sainnhe/sonokai",
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      vim.g.sonokai_style = "andromeda"
-      vim.g.sonokai_better_performance = 1
-      vim.cmd([[colorscheme sonokai]])
+      require("github-theme").setup({
+        -- ...
+      })
+
+      vim.cmd("colorscheme github_light")
     end,
   },
 }
