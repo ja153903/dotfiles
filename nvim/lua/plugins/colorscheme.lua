a4c3b72df9e5ece230d27fae13220be089328f58
ja153903 <jaimeabbariao@gmail.com>
2025-03-14 02:06:10 -0400

    going light again for a while

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,24 +1,10 @@
 return {
-  -- {
-  --   "sainnhe/sonokai",
-  --   config = function()
-  --     vim.g.sonokai_style = "andromeda"
-  --     vim.g.sonokai_better_performance = 1
-  --     vim.cmd([[colorscheme sonokai]])
-  --   end,
-  -- },
-
   {
-    "projekt0n/github-nvim-theme",
-    name = "github-theme",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    "Verf/deepwhite.nvim",
+    lazy = false,
+    priority = 1000,
     config = function()
-      require("github-theme").setup({
-        -- ...
-      })
-
-      vim.cmd("colorscheme github_light")
+      vim.cmd([[colorscheme deepwhite]])
     end,
   },
 }
