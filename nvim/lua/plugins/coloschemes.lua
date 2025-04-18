2b5f15219461401040b5fe92baf395bd31d613e8
ja153903 <jaimeabbariao@gmail.com>
2025-04-18 13:55:25 -0400

    updating colorscheme

--- nvim/lua/plugins/coloschemes.lua
+++ nvim/lua/plugins/coloschemes.lua
@@ -1,1 +1,10 @@
-return {}
+return {
+  -- lua/plugins/rose-pine.lua
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      vim.cmd("colorscheme rose-pine")
+    end,
+  },
+}
