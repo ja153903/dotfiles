105d71436cb3f29caae365a3ec3b7ecdbd5807be
ja153903 <jaimeabbariao@gmail.com>
2025-04-22 01:01:12 -0400

    updating lualine stuff

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -16,7 +16,7 @@ return {
     local lualine_require = require("lualine_require")
     lualine_require.require = require
 
-    local icons = LazyVim.config.icons
+    -- local icons = LazyVim.config.icons
 
     vim.o.laststatus = vim.g.lualine_laststatus
 
@@ -37,30 +37,30 @@ return {
           "branch",
           {
             "diagnostics",
-            symbols = {
-              error = icons.diagnostics.Error,
-              warn = icons.diagnostics.Warn,
-              info = icons.diagnostics.Info,
-              hint = icons.diagnostics.Hint,
-            },
+            -- symbols = {
+            --   error = icons.diagnostics.Error,
+            --   warn = icons.diagnostics.Warn,
+            --   info = icons.diagnostics.Info,
+            --   hint = icons.diagnostics.Hint,
+            -- },
           },
           {
             "diff",
-            symbols = {
-              added = icons.git.added,
-              modified = icons.git.modified,
-              removed = icons.git.removed,
-            },
-            source = function()
-              local gitsigns = vim.b.gitsigns_status_dict
-              if gitsigns then
-                return {
-                  added = gitsigns.added,
-                  modified = gitsigns.changed,
-                  removed = gitsigns.removed,
-                }
-              end
-            end,
+            -- symbols = {
+            --   added = icons.git.added,
+            --   modified = icons.git.modified,
+            --   removed = icons.git.removed,
+            -- },
+            -- source = function()
+            --   local gitsigns = vim.b.gitsigns_status_dict
+            --   if gitsigns then
+            --     return {
+            --       added = gitsigns.added,
+            --       modified = gitsigns.changed,
+            --       removed = gitsigns.removed,
+            --     }
+            --   end
+            -- end,
           },
         },
         lualine_x = {},
