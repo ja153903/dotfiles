30f09ae4bf5540ae16a303578841a4bc84c069f4
ja153903 <jaimeabbariao@gmail.com>
2025-07-27 01:46:19 -0400

    updating ghostty config

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -6,29 +6,20 @@ return {
     config = function()
       local c = require("vscode.colors").get_colors()
       require("vscode").setup({
-        -- Alternatively set style in setup
-        -- style = 'light'
-
         -- Enable transparent background
         transparent = true,
-
         -- Enable italic comment
         italic_comments = true,
-
         -- Underline `@markup.link.*` variants
         underline_links = true,
-
         -- Disable nvim-tree background color
         disable_nvimtree_bg = true,
-
         -- Apply theme colors to terminal
         terminal_colors = true,
-
         -- Override colors (see ./lua/vscode/colors.lua)
         color_overrides = {
           vscLineNumber = "#FFFFFF",
         },
-
         -- Override highlight groups (see ./lua/vscode/theme.lua)
         group_overrides = {
           -- this supports the same val table as vim.api.nvim_set_hl
@@ -36,9 +27,6 @@ return {
           Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
         },
       })
-      -- require('vscode').load()
-
-      -- load the theme without affecting devicon colors.
       vim.cmd.colorscheme("vscode")
     end,
   },
