6969133d79e73defe737470834f5c3e7c11d21f4
ja153903 <jaimeabbariao@gmail.com>
2025-07-28 01:04:28 -0400

    updating dotfiles

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,33 +1,13 @@
 return {
+  -- Lazy
   {
-    "Mofiqul/vscode.nvim",
-    lazy = false,
-    priority = 1000,
+    "vague2k/vague.nvim",
     config = function()
-      local c = require("vscode.colors").get_colors()
-      require("vscode").setup({
-        -- Enable transparent background
-        transparent = true,
-        -- Enable italic comment
-        italic_comments = true,
-        -- Underline `@markup.link.*` variants
-        underline_links = true,
-        -- Disable nvim-tree background color
-        disable_nvimtree_bg = true,
-        -- Apply theme colors to terminal
-        terminal_colors = true,
-        -- Override colors (see ./lua/vscode/colors.lua)
-        color_overrides = {
-          vscLineNumber = "#FFFFFF",
-        },
-        -- Override highlight groups (see ./lua/vscode/theme.lua)
-        group_overrides = {
-          -- this supports the same val table as vim.api.nvim_set_hl
-          -- use colors from this colorscheme by requiring vscode.colors!
-          Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
-        },
+      -- NOTE: you do not need to call setup if you don't want to.
+      require("vague").setup({
+        -- optional configuration here
       })
-      vim.cmd.colorscheme("vscode")
+      vim.cmd([[colorscheme vague]])
     end,
   },
 }
