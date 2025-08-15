181f6cfb014482cd9620ca4028e03a9360f65d6b
ja153903 <jaimeabbariao@gmail.com>
2025-08-14 22:30:15 -0400

    updating colorscheme to classic dark

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -162,7 +162,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd([[colorscheme base16-windows-95]])
+      vim.cmd([[colorscheme base16-classic-dark]])
     end,
   },
 }
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -18,7 +18,7 @@ config.font = wezterm.font_with_fallback({ "Monaspace Krypton", "MonaspiceKr Ner
 config.font_size = 12
 -- config.color_scheme = "Vs Code Dark+ (Gogh)"
 -- config.color_scheme = "GruvboxDark"
-
+config.color_scheme = "Classic Dark (base16)"
 config.leader = { key = "e", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
 	{
