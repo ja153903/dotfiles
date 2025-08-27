6439457772e79815f043914caec4b4bab5eb8c68
ja153903 <jaimeabbariao@gmail.com>
2025-08-27 00:21:16 -0400

    updating config

--- fish/config.fish
+++ fish/config.fish
@@ -18,8 +18,6 @@ if set -q VIRTUAL_ENV
     echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
 end
 
-starship init fish | source
-
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
