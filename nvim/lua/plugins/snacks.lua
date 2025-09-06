65c9db13be00a0f8be900a2390f88aaa8985c283
ja153903 <jaimeabbariao@gmail.com>
2025-09-06 13:55:08 -0400

    removing dashboard for now

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -10,7 +10,7 @@ return {
       -- refer to the configuration section below
       bigfile = { enabled = true },
       dashboard = {
-        enabled = true,
+        enabled = false,
       },
       indent = { enabled = false },
       input = { enabled = true },
