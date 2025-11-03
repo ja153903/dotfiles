10ec64b6e229fc4be2a16744c3deb1dfe1dc4706
ja153903 <jaimeabbariao@gmail.com>
2025-11-02 22:25:35 -0500

    we're back to using snacks

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -7,31 +7,31 @@ return {
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
-      -- bigfile = { enabled = true },
-      -- dashboard = {
-      --   enabled = false,
-      -- },
+      bigfile = { enabled = true },
+      dashboard = {
+        enabled = false,
+      },
       indent = { enabled = false },
-      -- input = { enabled = true },
+      input = { enabled = true },
       notifier = { enabled = false },
-      -- quickfile = { enabled = true },
-      -- scroll = { enabled = false },
-      -- statuscolumn = { enabled = true },
-      -- words = { enabled = true },
-      -- picker = {
-      --   layout = {
-      --     fullscreen = true,
-      --   },
-      --   sources = {
-      --     explorer = {
-      --       auto_close = true,
-      --     },
-      --   },
-      -- },
-      -- scope = { enabled = true },
+      quickfile = { enabled = true },
+      scroll = { enabled = false },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+      picker = {
+        layout = {
+          fullscreen = true,
+        },
+        sources = {
+          explorer = {
+            auto_close = true,
+          },
+        },
+      },
+      scope = { enabled = true },
       animate = { enabled = true },
-      -- images = { enabled = false },
-      -- terminal = { enabled = true },
+      images = { enabled = false },
+      terminal = { enabled = true },
     },
   },
 }
