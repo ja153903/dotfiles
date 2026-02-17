f1dc429675f75263e0094566193ea6b7d3e5e617
ja153903 <jaimeabbariao@gmail.com>
2026-02-16 21:10:05 -0500

    updating with new colorschemes

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,67 +1,27 @@
 return {
   {
-    "craftzdog/solarized-osaka.nvim",
-    -- lazy = false,
-    -- priority = 1000,
-    enabled = false,
+    "everviolet/nvim",
+    name = "evergarden",
+    priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
+    opts = {
+      theme = {
+        variant = "fall", -- 'winter'|'fall'|'spring'|'summer'
+        accent = "green",
+      },
+      editor = {
+        transparent_background = false,
+        sign = { color = "none" },
+        float = {
+          color = "mantle",
+          solid_border = false,
+        },
+        completion = {
+          color = "surface0",
+        },
+      },
+    },
     config = function()
-      require("solarized-osaka").setup({
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        transparent = true, -- Enable this to disable setting the background color
-        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
-        styles = {
-          -- Style to be applied to different syntax groups
-          -- Value is any valid attr-list value for `:help nvim_set_hl`
-          comments = { italic = true },
-          keywords = { italic = true },
-          functions = {},
-          variables = {},
-          -- Background styles. Can be "dark", "transparent" or "normal"
-          sidebars = "transparent", -- style for sidebars, see below
-          floats = "transparent", -- style for floating windows
-        },
-        sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-        day_brightness = 0.7, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-        hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-        dim_inactive = false, -- dims inactive windows
-        lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
-      })
-      -- vim.cmd([[colorscheme solarized-osaka]])
-    end,
-  },
-  {
-    "loctvl842/monokai-pro.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("monokai-pro").setup({
-        transparent_background = true,
-        terminal_colors = true,
-        devicons = true,
-        styles = {
-          comment = { italic = true },
-          keyword = { italic = true },
-          type = { italic = true },
-          storageclass = { italic = true },
-          structure = { italic = true },
-          parameter = { italic = true },
-          annotation = { italic = true },
-          tag_attribute = { italic = true },
-        },
-        filter = "classic", -- classic | octagon | pro | machine | ristretto | spectrum
-        inc_search = "underline", -- underline | background
-        background_clear = {
-          "toggleterm",
-          "telescope",
-          "renamer",
-          "notify",
-          "snacks",
-          "mini",
-        },
-      })
-
-      vim.cmd.colorscheme("monokai-pro")
+      vim.cmd([[colorscheme evergarden]])
     end,
   },
 }
