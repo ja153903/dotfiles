e1d21576b033dae3036e8feedf3d47981cf5e625
ja153903 <jaimeabbariao@gmail.com>
2026-04-03 13:41:34 -0400

    i guess im trying these out

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,7 +4,11 @@
 vim.hl = vim.highlight
 vim.opt.list = false
 vim.opt.termguicolors = true
-
+vim.opt.guicursor = {
+  "n-v-c-sm:block", -- normal, visual, command: steady block
+  "i-ci-ve:ver25-blinkon500-blinkoff500-blinkwait500", -- insert: blinking vertical bar
+  "r-cr-o:hor20", -- replace, operator-pending: steady horizontal bar
+}
 -- Source - https://stackoverflow.com/a/78627671
 -- Posted by Daniel Macak, modified by community. See post 'Timeline' for change history
 -- Retrieved 2026-01-30, License - CC BY-SA 4.0
