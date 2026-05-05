72469a1fd34848e270fdd1442560465709670be7
ja153903 <jaimeabbariao@gmail.com>
2026-05-05 02:19:50 -0400

    updating neovim config

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,17 +1,9 @@
 return {
-  {
-    "tanvirtin/monokai.nvim",
-  },
   { "EdenEast/nightfox.nvim" }, -- lazy
-  {
-    "oskarnurm/koda.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "koda-glade",
+      colorscheme = "carbonfox",
     },
   },
 }
