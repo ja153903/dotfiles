3ac4a85c3775e551a82ca7376b7b068e23d9e536
ja153903 <jaimeabbariao@gmail.com>
2026-06-01 20:59:44 -0400

    updated snacks file

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -11,6 +11,22 @@ return {
       animate = { enabled = true },
       indent = { enabled = false },
       dashboard = { enabled = false },
+      picker = {
+        layout = {
+          layout = {
+            backdrop = false,
+            row = 0,
+            col = 0,
+            width = 0,
+            height = 0,
+            border = "none",
+            box = "vertical",
+            { win = "input", height = 1, border = "bottom" },
+            { win = "list", flex = 1, border = "none" },
+            { win = "preview", height = 0.3, border = "top" },
+          },
+        },
+      },
     },
   },
 }
