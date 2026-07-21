fe5e177748d00c5f230267c72f56a918fe19f7df
ja153903 <jaimeabbariao@gmail.com>
2026-07-21 00:17:19 -0400

    feat: running local colorschemes

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,6 +1,8 @@
 return {
   {
     "ja153903/embark-theme-vim",
+    dir = vim.fn.expand("~/programming/oss/embark-theme-vim"),
+    dev = true,
     lazy = false,
     priority = 1000,
     name = "embark",
