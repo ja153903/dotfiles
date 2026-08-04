5075191dc36c7f7de13b09d3decf09788ead981d
ja153903 <jaimeabbariao@gmail.com>
2026-08-03 22:29:02 -0400

    fix: updating with proper commit history

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,11 +6,12 @@ local config = wezterm.config_builder()
 
 local scheme = wezterm.color.load_scheme(wezterm.config_dir .. "/colors/modus_vivendi.toml")
 
-config.font = wezterm.font("Monaspace Krypton")
-config.color_scheme_dirs = { wezterm.config_dir .. "/colors" }
-config.color_scheme = "embark"
-config.window_decorations = "RESIZE"
+config.font_dirs = { wezterm.home_dir .. "/Library/Fonts" }
+config.font = wezterm.font("PlemolJP Console NF")
 config.line_height = 1.4
+config.color_scheme_dirs = { wezterm.config_dir .. "/colors" }
+config.color_scheme = "Solarized Dark (Gogh)"
+config.window_decorations = "RESIZE"
 
 -- Performance settings
 config.max_fps = 144
