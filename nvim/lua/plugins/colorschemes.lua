4039eb23f97b052dd8126c66e772bb41d6102982
ja153903 <jaimeabbariao@gmail.com>
2026-08-07 00:18:01 -0400

    feat: updating wezterm colorscheme stuff

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,12 +1,12 @@
 return {
-  -- {
-  --   "ja153903/embark-theme-vim",
-  --   dir = vim.fn.expand("~/programming/oss/embark-theme-vim"),
-  --   dev = true,
-  --   lazy = false,
-  --   priority = 1000,
-  --   name = "embark",
-  -- },
+  {
+    "ja153903/embark-theme-vim",
+    dir = vim.fn.expand("~/programming/oss/embark-theme-vim"),
+    dev = true,
+    lazy = false,
+    priority = 1000,
+    name = "embark",
+  },
   {
     "craftzdog/solarized-osaka.nvim",
     lazy = false,
@@ -16,7 +16,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "solarized-osaka",
+      colorscheme = "embark",
     },
   },
 }
