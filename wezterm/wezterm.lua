42949df0754078046220256126befba3a15f5fed
ja153903 <jaimeabbariao@gmail.com>
2026-08-17 21:13:20 -0400

    feat: updating some wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,7 +8,6 @@ local scheme = wezterm.color.load_scheme(wezterm.config_dir .. "/colors/modus_vi
 
 config.font_dirs = { wezterm.home_dir .. "/Library/Fonts" }
 config.font = wezterm.font("TX-02")
-config.font_size = 15
 config.color_scheme_dirs = { wezterm.config_dir .. "/colors" }
 config.color_scheme = "embark"
 config.window_decorations = "RESIZE"
